3a040b5275e1013f0253b11db62a7c06c80a8e0d
sqrtxander <xanderjames128@gmail.com>
2023-11-10 17:07:58 +1100

    day05

---
+++ day05/part2.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+import re
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.in')
+
+
+def solve(s: str) -> int:
+
+    lens = set()
+
+    for c in set(s.lower()):
+        new_s = s.replace(c, '').replace(c.upper(), '')
+
+        lst = list(new_s.strip())
+        prev_len = 0
+        while len(lst) != prev_len:
+            prev_len = len(lst)
+            lst = react(lst)
+        lens.add(len(lst))
+    return min(lens)
+
+
+def react(lst: list[int]) -> list[int]:
+    i = 0
+    while i < len(lst) - 1:
+        if lst[i] != lst[i + 1] and lst[i].lower() == lst[i + 1].lower():
+            del lst[i:i + 2]
+            i = max(0, i - 1)
+        else:
+            i += 1
+    return lst
+
+
+INPUT_S = '''\
+dabAcCaCBAcCcaDA
+'''
+EXPECTED = 4
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert solve(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(solve(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
