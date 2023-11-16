8a2c944cb0efdd804af628b2b28bfe77b1b3f5da
sqrtxander <xanderjames128@gmail.com>
2023-11-16 18:20:26 +1100

    day10

---
+++ day10/part1.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+import re
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.in')
+
+
+class Point:
+    def __init__(self, x: int, y: int, dx: int, dy: int) -> None:
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+
+    def update(self) -> None:
+        self.x += self.dx
+        self.y += self.dy
+
+    def reverse(self) -> None:
+        self.x -= self.dx
+        self.y -= self.dy
+
+
+def solve(s: str) -> int:
+    points = []
+    for line in s.splitlines():
+        x_s, y_s, dx_s, dy_s = re.match(
+            r'position=<\s*(-?\d+),\s*(-?\d+)> velocity=<\s*(-?\d+),\s*(-?\d+)>', line).groups()
+        points.append(Point(int(x_s), int(y_s), int(dx_s), int(dy_s)))
+
+    min_y = min(point.y for point in points)
+    max_y = max(point.y for point in points)
+
+    height = max_y - min_y
+    prev_height = height + 1
+    while height < prev_height:
+        prev_height = height
+        for point in points:
+            point.update()
+
+        min_y = min(point.y for point in points)
+        max_y = max(point.y for point in points)
+
+        height = max_y - min_y
+
+    for point in points:
+        point.reverse()
+    points_tuple = {(point.x, point.y) for point in points}
+    return support.format_coords_hash(points_tuple)
+
+
+INPUT_S = '''\
+position=< 9,  1> velocity=< 0,  2>
+position=< 7,  0> velocity=<-1,  0>
+position=< 3, -2> velocity=<-1,  1>
+position=< 6, 10> velocity=<-2, -1>
+position=< 2, -4> velocity=< 2,  2>
+position=<-6, 10> velocity=< 2, -2>
+position=< 1,  8> velocity=< 1, -1>
+position=< 1,  7> velocity=< 1,  0>
+position=<-3, 11> velocity=< 1, -2>
+position=< 7,  6> velocity=<-1, -1>
+position=<-2,  3> velocity=< 1,  0>
+position=<-4,  3> velocity=< 2,  0>
+position=<10, -3> velocity=<-1,  1>
+position=< 5, 11> velocity=< 1, -2>
+position=< 4,  7> velocity=< 0, -1>
+position=< 8, -2> velocity=< 0,  1>
+position=<15,  0> velocity=<-2,  0>
+position=< 1,  6> velocity=< 1,  0>
+position=< 8,  9> velocity=< 0, -1>
+position=< 3,  3> velocity=<-1,  1>
+position=< 0,  5> velocity=< 0, -1>
+position=<-2,  2> velocity=< 2,  0>
+position=< 5, -2> velocity=< 1,  2>
+position=< 1,  4> velocity=< 2,  1>
+position=<-2,  7> velocity=< 2, -2>
+position=< 3,  6> velocity=<-1, -1>
+position=< 5,  0> velocity=< 1,  0>
+position=<-6,  0> velocity=< 2,  0>
+position=< 5,  9> velocity=< 1, -2>
+position=<14,  7> velocity=<-2,  0>
+position=<-3,  6> velocity=< 2, -1>
+'''
+EXPECTED = '''\
+#   #  ###
+#   #   # 
+#   #   # 
+#####   # 
+#   #   # 
+#   #   # 
+#   #   # 
+#   #  ###'''
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert solve(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(solve(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
